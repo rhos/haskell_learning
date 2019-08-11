3af456a23ae18d7d859615fe91bfa41a2bb70254
ilyas tazitdinov <itazitdinov@acumatica.com>
2019-08-11 19:19:42 +0300

    noaa

--- unit7/noaa/app/Main.hs
+++ unit7/noaa/app/Main.hs
@@ -13,11 +13,43 @@ noaaHost = "www.ncdc.noaa.gov"
 apiPath :: BC.ByteString
 apiPath = "/cdo-web/api/v2/datasets"
 
+buildRequest
+  :: BC.ByteString -> BC.ByteString -> BC.ByteString -> BC.ByteString -> Request
+buildRequest token host method path =
+  setRequestMethod method
+  . setRequestHost host
+  . setRequestHeader "token" [token]
+  . setRequestPath path
+  . setRequestSecure True
+  . setRequestPort 443
+  $ defaultRequest
+
+buildRequestNOSSL :: BC.ByteString -> BC.ByteString
+  -> BC.ByteString -> BC.ByteString -> Request
+buildRequestNOSSL token host method path = setRequestMethod method
+  $ setRequestHost host
+  $ setRequestHeader "token" [token]
+  $ setRequestSecure False
+  $ setRequestPort 80
+  $ setRequestPath path
+  $ defaultRequest
+
+request :: Request
+request = buildRequest myToken noaaHost "GET" apiPath
+
 main :: IO ()
-main = return ()
+main = do
+  response <- httpLBS request
+  let status = getResponseStatusCode response
+  if status == 200
+    then do
+      print "saving request to file"
+      let jsonBody = getResponseBody response
+      L.writeFile "data.json" jsonBody
+    else print "request failed with error"
 
-responseM :: IO (Response LC.ByteString)
-responseM = httpLBS "http://news.ycombinator.com"
+-- responseM :: IO (Response LC.ByteString)
+-- responseM = httpLBS "http://news.ycombinator.com"
 
-response :: IO Int
-response = getResponseStatusCode <$> responseM
+-- response :: IO Int
+-- response = getResponseStatusCode <$> responseM
